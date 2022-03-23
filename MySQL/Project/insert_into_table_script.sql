38d19aba3507dc9ff1a85381ef3f7592df67df29
annamalai-sam <annamalai.capitalsam@gmail.com>
2022-03-23 11:46:48 +0530

    push

--- MySQL/Project/insert_into_table_script.sql
+++ MySQL/Project/insert_into_table_script.sql
@@ -1,25 +1,19 @@
 USE freshschool;
 
-insert into 
-roles
-(name) 
-values
+insert into roles
+(name) values
 ("admin"),
 ("applicant");
 
-insert into 
-faq
-(question,mail,name) 
-values
+insert into faq
+(question,mail,name) values
 ("What is the course fee?","test1@abc.com","test1"),
 ("What is the duration of the program?","test2@abc.com","test2"),
 ("Can I reapply?","test3@abc.com","test3"),
 ("What is expected of me during the FWSA programme?","test4@abc.com","test4");
 
-insert into 
-users
-(role_id,first_name,last_name,email,contact_number,date_of_birth,Gender,parent_name,parent_contact_number,address) 
-values
+insert into users
+(role_id,first_name,last_name,email,contact_number,date_of_birth,Gender,parent_name,parent_contact_number,address) values
 ("1","anna","malai","test1@abc.com","9876543102","2006-03-01 00:00:00","Male","malai","9876543210","madurai"),
 ("1","chith","raj","test2@abc.com","8765432109","2006-03-01 00:00:00","Female","raj","9876543210","tirunelveli"),
 ("2","test","one","testone@abc.com","9776543201","2006-03-01 00:00:00","Male","one","9876543210","madurai"),
@@ -34,14 +28,12 @@ values
 ("2","test","ten","testten@abc.com","9976543401","2006-03-01 00:00:00","Female","ten","9876543210","chennai");
 
 insert into admin_details
-(user_id,password) 
-values
+(user_id,password) values
 ("1","passw@01"),
 ("2","passw@02");
 
 insert into application_status
-(user_id,result) 
-values
+(user_id,result) values
 ("3","pending"),
 ("4","selected"),
 ("5","pending"),
@@ -55,8 +47,7 @@ values
 
 
 insert into application_result_checking_details 
-(user_id)
-values 
+(user_id) values 
 (3),
 (4),
 (5);
