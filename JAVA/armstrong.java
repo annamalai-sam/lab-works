7bf8438837de85bb0505244b103545ec28578a39
annamalai-sam <annamalai.capitalsam@gmail.com>
2022-04-13 12:29:11 +0530

    added armstrong program

---
+++ JAVA/armstrong.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class armstrong {
+    public static void main(String... s) {
+        Scanner ObjScanner = new Scanner(System.in);
+        System.out.print("Enter a postive number : ");
+        int num = ObjScanner.nextInt();
+        // System.out.println(num);
+        int powerValue = 0;
+        int armstrongValue = 0;
+        int number = num;
+        int finalNumber = num;
+        // System.out.println(finalNumber);
+        while (number != 0) {
+            number = number / 10;
+            powerValue++;
+        }
+        while (num != 0) {
+            int remainder = 0;
+            remainder = num % 10;
+            // System.out.println(remainder);
+            int power = 1;
+            for (int i = 0; i < powerValue; i++) {
+                power = power * remainder;
+                // System.out.println(power);
+            }
+            num = num / 10;
+            armstrongValue = armstrongValue + power;
+        }
+        System.out.println("armstrongValue : " + armstrongValue);
+        if (armstrongValue == finalNumber) {
+            System.out.println(finalNumber + "  is Armstrong number");
+        } else {
+            System.out.println(finalNumber + "  is not a Armstrong number");
+        }
+
+    }
+}
