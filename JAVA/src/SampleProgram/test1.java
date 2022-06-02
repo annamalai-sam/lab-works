0bca6f364e45856105b9dd1321098b3c491b4e90
annamalai-sam <annamalai190803@gmail.com>
2022-06-02 11:33:28 +0530

    Added java files

--- JAVA/src/SampleProgram/test1.java
+++ JAVA/src/SampleProgram/test1.java
@@ -6,16 +6,27 @@ class  sample {
      sample(){
         System.out.println("In constructor");
     }
+    void myMethod1(){
+        System.out.println("myMethod1");
+    }
 
+    void myMethod(){
+         sample onj = new sample();
+         onj.myMethod1();
+        System.out.println("Annamalai");
+
+    }
     public static void main(String[] args) {
-        Scanner scan1  = new Scanner(System.in);
-        int a1 = scan1.nextInt();
-        Scanner scan2  = new Scanner(System.in);
-        int a2 = scan2.nextInt();
-        Scanner scan3  = new Scanner(System.in);
-        int a3 = scan3.nextInt();
-        System.out.println(a1);
-        System.out.println(a2);
-        System.out.println(a3);
+//        Scanner scan1  = new Scanner(System.in);
+//        int a1 = scan1.nextInt();
+//        Scanner scan2  = new Scanner(System.in);
+//        int a2 = scan2.nextInt();
+//        Scanner scan3  = new Scanner(System.in);
+//        int a3 = scan3.nextInt();
+//        System.out.println(a1);
+//        System.out.println(a2);
+//        System.out.println(a3);
+        sample onj = new sample();
+        onj.myMethod();
     }
 }
